968d2ac1a4bda57cfa965b559fe959422e99d80e
Monoblade <fushersf2@gmail.com>
2024-03-07 17:53:08 +0300

    Fix visual bugs and new improvement

--- src/script/main-script.js
+++ src/script/main-script.js
@@ -23,8 +23,8 @@ document.querySelector('.improvement-btn').addEventListener('click', () => {
     displayMoney();
     document.querySelector(".improvement-text-one").innerHTML = `Кам'яне покращення`;
     document.querySelector(".improvement-text-two").innerHTML = `3000 монет`;
-    document.querySelector(".improvement-text-four").innerHTML = `За 1 клік +2 монети`
-    document.querySelector(".improvement-text-three").innerHTML = `В одну секунду +1 монета`
+    document.querySelector(".improvement-text-four").innerHTML = `За 1 клік +5 монети`
+    document.querySelector(".improvement-text-three").innerHTML = `В одну секунду +2 монета`
     document.querySelector(".indexes-text-one").innerHTML = `За 1 клік - ${addMoney} монет`;
     document.querySelector(".indexes-text-two").innerHTML = `За 1 секунду - ${autoAddMoney} монет`;
   } else if (money >= 3000 && improvementUpdateCounter === 2) {
@@ -35,8 +35,8 @@ document.querySelector('.improvement-btn').addEventListener('click', () => {
     displayMoney();
     document.querySelector(".improvement-text-one").innerHTML = `Бронзове покращення`;
     document.querySelector(".improvement-text-two").innerHTML = `10000 монет`;
-    document.querySelector(".improvement-text-four").innerHTML = `За 1 клік +5 монет`
-    document.querySelector(".improvement-text-three").innerHTML = `В одну секунду +2 монети`
+    document.querySelector(".improvement-text-four").innerHTML = `За 1 клік +10 монет`
+    document.querySelector(".improvement-text-three").innerHTML = `В одну секунду +4 монети`
     document.querySelector(".indexes-text-one").innerHTML = `За 1 клік - ${addMoney} монет`;
     document.querySelector(".indexes-text-two").innerHTML = `За 1 секунду - ${autoAddMoney} монет`;
     
@@ -47,9 +47,21 @@ document.querySelector('.improvement-btn').addEventListener('click', () => {
     autoAddMoney += 4;
     displayMoney();
     document.querySelector(".improvement-text-one").innerHTML = `Мідне покращення`;
-    document.querySelector(".improvement-text-two").innerHTML = `10000 монет`;
-    document.querySelector(".improvement-text-four").innerHTML = `За 1 клік +10 монет`
-    document.querySelector(".improvement-text-three").innerHTML = `В одну секунду +4 монети`
+    document.querySelector(".improvement-text-two").innerHTML = `30000 монет`;
+    document.querySelector(".improvement-text-four").innerHTML = `За 1 клік +20 монет`
+    document.querySelector(".improvement-text-three").innerHTML = `В одну секунду +8 монети`
+    document.querySelector(".indexes-text-one").innerHTML = `За 1 клік - ${addMoney} монет`;
+    document.querySelector(".indexes-text-two").innerHTML = `За 1 секунду - ${autoAddMoney} монет`;
+  } else if (money >= 30000 && improvementUpdateCounter === 4) {
+    improvementUpdateCounter += 1;
+    money -= 30000;
+    addMoney += 20;
+    autoAddMoney += 8;
+    displayMoney();
+    document.querySelector(".improvement-text-one").innerHTML = `Олов'яне покращення`;
+    document.querySelector(".improvement-text-two").innerHTML = `70000 монет`;
+    document.querySelector(".improvement-text-four").innerHTML = `За 1 клік +20 монет`
+    document.querySelector(".improvement-text-three").innerHTML = `В одну секунду +8 монети`
     document.querySelector(".indexes-text-one").innerHTML = `За 1 клік - ${addMoney} монет`;
     document.querySelector(".indexes-text-two").innerHTML = `За 1 секунду - ${autoAddMoney} монет`;
   }
